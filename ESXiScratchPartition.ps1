faa65b305734dc5a93ad7fc257ca68c1fccae0e5
red56gmc <red56gmc@gmail.com>
2017-01-10 15:54:21 -0800

    Update ESXiScratchPartition.ps1

--- ESXiScratchPartition.ps1
+++ ESXiScratchPartition.ps1
@@ -6,6 +6,11 @@ $scratch = “ScratchConfig.ConfiguredScratchLocation”
 Foreach ($esx in $vmhosts) {Get-AdvancedSetting -Entity $esx -Name $scratch | Select @{Name="ESXi Host";Expression={$esx.Name}}, Value} 
 
 
-#This section lists the path for the selected datastores
+#This command lists the path for the selected datastores
 
 Get-View -ViewType Datastore -filter @{'name'='sdlc'} | Select -ExpandProperty Info | Select Name, URL | Ft
+
+
+##This is the command to update the location of the Scratch Partition. Once updated the ESXi host will have to be rebooted for the changes to take effect.
+
+Get-AdvancedSetting -Entity hqsdlcesxi04* -Name $scratch | Set-AdvancedSetting -Value /vmfs/volumes/57755694-749749f8-acbd-0025b5a31000
